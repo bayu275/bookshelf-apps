9c63730ac831888dbd789a82bd815551c0fd1002
Bayu <bayuusetiawan86@gmail.com>
2024-02-25 22:21:39 +0700

    style(UI): :lipstick: update header and add book feature
    Refine the styling to enhance overall visual appeal and consistency

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+const $ = (selector) => {
+    const element = document.querySelectorAll(selector);
+    return element.length === 1 ? element[0] : element;
+};
 
+const checkbox = $('#read');
+
+checkbox.addEventListener('click', function () {
+    checkbox.classList.toggle('checked');
+});
+
+checkbox.addEventListener('keydown', function (event) {
+    const { key } = event;
+    if (key === 'Enter') {
+        checkbox.classList.toggle('checked');
+    }
+});
